2b7732207f3940a3b1cabc635e8932e9522167bb
Adeel Arif Bhatti <adeel.bhatti@huddly.com>
2022-07-08 15:09:13 +0200

    Maps

--- map1.go
+++ map1.go
@@ -8,12 +8,22 @@ func main() {
 	"ur" : "Urdu"}
 	fmt.Println(codes["en"])
 	fmt.Println(codes["ur"])
+	// Adding a key
 	codes["it"] = "Italian"
+	// checking if the value exists
 	value, found := codes["fr"]
 	fmt.Println(found,value)
 	fmt.Println(codes)
-
+	// deleting the value
 	delete(codes,"ur")
 	fmt.Println(codes)
+	//for loop for the key/value
+	for key, value := range codes {
+		fmt.Println(key, "=>" ,value)
+		// deleting the map
+		// delete(codes,key)
+		// or another method is to reinitial the map
+		// codes = make(map[string]string)
+	}
 
 }
